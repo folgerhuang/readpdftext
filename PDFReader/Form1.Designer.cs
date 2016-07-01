3ea25f90876c206d3067623a2e1f7aa9e469189a
folgerhuang <379771379@qq.com>
2016-07-01 10:12:09 +0800

    Add project files.

---
+++ PDFReader/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace PDFReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnOpenPDF = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(753, 417);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnOpenPDF
+            // 
+            this.btnOpenPDF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenPDF.Location = new System.Drawing.Point(690, 435);
+            this.btnOpenPDF.Name = "btnOpenPDF";
+            this.btnOpenPDF.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenPDF.TabIndex = 1;
+            this.btnOpenPDF.Text = "&Open";
+            this.btnOpenPDF.UseVisualStyleBackColor = true;
+            this.btnOpenPDF.Click += new System.EventHandler(this.btnOpenPDF_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 458);
+            this.Controls.Add(this.btnOpenPDF);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "PDF Reader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnOpenPDF;
+    }
+}
+
---
+++ PDFReader/Form1.cs
@@ -0,0 +1,47 @@
+﻿using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenPDF_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() { Filter="PDF files|*.pdf",ValidateNames=true,Multiselect=false})
+            {
+                if(ofd.ShowDialog()==DialogResult.OK)
+                {
+                    try
+                    {
+                        iTextSharp.text.pdf.PdfReader read = new iTextSharp.text.pdf.PdfReader(ofd.FileName);
+                        StringBuilder sb = new StringBuilder();
+                        for (int i = 1; i <= read.NumberOfPages; i++)
+                        {
+                            sb.Append(PdfTextExtractor.GetTextFromPage(read, i));
+                        }
+                        richTextBox1.Text = sb.ToString();
+                        read.Close();
+                    }
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show(ex.Message,"Message",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
